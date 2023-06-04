3d2c3439e8e41fee8c5509c6b5dadca9cec05bc4
chinmayirage <chinmayirage37@gmail.com>
2023-06-04 21:27:31 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def manu(request):
+   return HttpResponse('<h1>this is specific url mapping<h1>')
